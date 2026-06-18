bcf25738e5459d331cbb5303e988628ac0801740
Venceslas Burongu <bvenceslas@gmail.com>
2026-06-18 15:39:07 +0200

    work around getting the declared at location name for declared action

--- src/analytics/analytics-precalculations.ts
+++ src/analytics/analytics-precalculations.ts
@@ -1,6 +1,8 @@
 import { differenceInDays } from 'date-fns'
 import { ActionDocument, AddressFieldValue } from '@opencrvs/toolkit/events'
 import { COUNTRY_NAMES_BY_CODE } from './countries'
+import { getClient } from './postgres'
+import { UUID } from 'crypto'
 
 function getCountryPlaceOfBirthResolved(
   declaration: ActionDocument['declaration']
@@ -151,9 +153,10 @@ function extractDiseaseFromSelectedCodes(
   return 'None'
 }
 
-export function precalculateDeathEvent(
+export async function precalculateDeathEvent(
   _action: ActionDocument,
-  declaration: ActionDocument['declaration']
+  declaration: ActionDocument['declaration'],
+  declaredAtLocation?: UUID
 ) {
   const ucCode = declaration['irisOutput.ucCode']
   const selectedCodes = declaration['irisOutput.selectedCodes']
@@ -168,13 +171,39 @@ export function precalculateDeathEvent(
     declaration
   )
 
+  const dbClient = getClient()
+  // const dbResponse = declaredAtLocation
+  // ? (
+  //     await dbClient
+  //       .selectFrom('locations')
+  //       .select('name')
+  //       .where('id', '=', declaredAtLocation)
+  //       .execute()
+  //   )[0]?.name
+  // : ''
+
+  const dbResponse = await dbClient
+    .selectFrom('locations')
+    .select('name')
+    .where('id', '=', declaredAtLocation)
+    .execute()
+
+  console.log('dbResponse :>> ', dbResponse)
+  const declaredAtLocationName = dbResponse[0]?.name || ''
+
+  if (!declaredAtLocationName) {
+    console.log('declaredAtLocation :>> ', declaredAtLocation)
+    console.log('declaredAtLocationName :>> ', declaredAtLocationName)
+  }
+
   return {
     ...declaration,
-    'irisOutput.ucCodeLabel': ucCodeLabel
+    'irisOutput.ucCodeLabel': ucCodeLabel,
+    declaredAtLocationName
   }
 }
 
-export function precalculateBirthEvent(
+export async function precalculateBirthEvent(
   action: ActionDocument,
   declaration: ActionDocument['declaration']
 ) {
--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -22,7 +22,8 @@ import {
   EventState,
   getActionAnnotationFields,
   getCurrentEventState,
-  Location
+  Location,
+  UUID
 } from '@opencrvs/toolkit/events'
 
 import { ExpressionBuilder, Kysely, sql } from 'kysely'
@@ -102,19 +103,20 @@ function getAnnotation(
   }
 }
 
-function precalculateAdditionalAnalytics(
+async function precalculateAdditionalAnalytics(
   action: ActionDocument,
   declaration: ActionDocument['declaration'],
-  eventConfig: EventConfig
+  eventConfig: EventConfig,
+  declaredAtLocation?: UUID
 ) {
   /*
    * Example: precalculate age from action creation date and child's date of birth
    */
   if (eventConfig.id === Event.Birth) {
-    return precalculateBirthEvent(action, declaration)
+    return await precalculateBirthEvent(action, declaration)
   }
   if (eventConfig.id === Event.Death) {
-    return precalculateDeathEvent(action, declaration)
+    return await precalculateDeathEvent(action, declaration, declaredAtLocation)
   }
 
   return declaration
@@ -134,6 +136,130 @@ function convertDotKeysToUnderscore(
 }
 
 type ActionDocWithId = Record<string, unknown>
+// async function upsertAnalyticsEventActions(
+//   events: EventDocument[],
+//   trx: Kysely<any>
+// ) {
+//   await trx
+//     .deleteFrom('analytics.event_actions')
+//     .where(
+//       'event_id',
+//       'in',
+//       events.map((e) => e.id)
+//     )
+//     .execute()
+
+//   const allEventActions: ActionDocWithId[] = []
+//   for (const event of events) {
+//     const eventConfig = getEventConfig(event.type)
+//     for (let i = 0; i < event.actions.length; i++) {
+//       const actionsFromStartToCurrentPoint = event.actions
+//         .sort((a, b) => {
+//           // CREATE type always comes first
+//           if (a.type === ActionType.CREATE && b.type !== ActionType.CREATE)
+//             return -1
+//           if (b.type === ActionType.CREATE && a.type !== ActionType.CREATE)
+//             return 1
+//           // Otherwise sort by createdAt
+//           return a.createdAt.localeCompare(b.createdAt)
+//         })
+//         .slice(0, i + 1)
+
+//       const action = event.actions[i]
+
+//       if (
+//         action.status === ActionStatus.Requested ||
+//         action.status === ActionStatus.Rejected
+//       ) {
+//         continue
+//       }
+
+//       const actionAtCurrentPoint = getCurrentEventState(
+//         {
+//           ...event,
+//           actions: actionsFromStartToCurrentPoint
+//         },
+//         eventConfig
+//       )
+
+//       const { type, ...act } = action
+
+//       const actionConfig = eventConfig.actions.find((a) => a.type === type)
+
+//       const annotation = actionConfig
+//         ? pickAnnotationAnalyticsFields(
+//             getAnnotation(action, event.actions),
+//             actionConfig
+//           )
+//         : {}
+
+//       const actions = event.actions
+//       /*
+//        * Add date of declaration and date of registration to all events for each access
+//        */
+//       const declareAction = actions.find((a) => a.type === ActionType.DECLARE)
+//       const registerAction = actions.find((a) => a.type === ActionType.REGISTER)
+
+//       const actionWithFilteredDeclaration = {
+//         ...act,
+//         eventId: event.id,
+//         actionType: type,
+//         eventType: event.type,
+//         declaredAt: declareAction ? declareAction.createdAt : null,
+//         registeredAt: registerAction ? registerAction.createdAt : null,
+//         annotation: convertDotKeysToUnderscore(annotation),
+//         declaration: convertDotKeysToUnderscore(
+//           await precalculateAdditionalAnalytics(
+//             action,
+//             pickDeclarationAnalyticsFields(
+//               actionAtCurrentPoint.declaration,
+//               eventConfig
+//             ),
+//             eventConfig,
+//             declareAction?.createdAtLocation
+//           )
+//         )
+//       }
+
+//       allEventActions.push(actionWithFilteredDeclaration)
+//     }
+//   }
+
+//   console.log(
+//     'allEventActions :>> ',
+//     allEventActions
+//       .filter((a) => a.actionType === ActionType.DECLARE)
+//       .map((act: any) => act.declaration.declaredAtLocationName as any)
+//   ) // Only log DECLARE actions as they are the most important ones for analytics and usually the most enriched ones
+
+//   if (allEventActions.length === 0) {
+//     return []
+//   }
+
+//   /*
+//    * This must be chunked not to cause an error if one event happens to have
+//    * 10k actions
+//    */
+//   const chunks = chunk(allEventActions, 3000)
+
+//   for (const batch of chunks) {
+//     await trx
+//       .insertInto('analytics.event_actions')
+//       .values(batch)
+//       .onConflict((oc) =>
+//         oc.column('id').doUpdateSet((eb) =>
+//           Object.fromEntries(
+//             Object.keys(allEventActions[0])
+//               .filter((key) => key !== 'id')
+//               .map((key) => [key, eb.ref(`excluded.${key}`)])
+//           )
+//         )
+//       )
+//       .execute()
+//   }
+//   return
+// }
+
 async function upsertAnalyticsEventActions(
   events: EventDocument[],
   trx: Kysely<any>
@@ -148,8 +274,10 @@ async function upsertAnalyticsEventActions(
     .execute()
 
   const allEventActions: ActionDocWithId[] = []
+
   for (const event of events) {
     const eventConfig = getEventConfig(event.type)
+
     for (let i = 0; i < event.actions.length; i++) {
       const actionsFromStartToCurrentPoint = event.actions
         .sort((a, b) => {
@@ -158,6 +286,7 @@ async function upsertAnalyticsEventActions(
             return -1
           if (b.type === ActionType.CREATE && a.type !== ActionType.CREATE)
             return 1
+
           // Otherwise sort by createdAt
           return a.createdAt.localeCompare(b.createdAt)
         })
@@ -192,11 +321,35 @@ async function upsertAnalyticsEventActions(
         : {}
 
       const actions = event.actions
+
       /*
        * Add date of declaration and date of registration to all events for each access
        */
       const declareAction = actions.find((a) => a.type === ActionType.DECLARE)
       const registerAction = actions.find((a) => a.type === ActionType.REGISTER)
+
+      const declarationAnalytics = await precalculateAdditionalAnalytics(
+        action,
+        pickDeclarationAnalyticsFields(
+          actionAtCurrentPoint.declaration,
+          eventConfig
+        ),
+        eventConfig,
+        declareAction?.createdAtLocation
+      )
+
+      if (type === ActionType.DECLARE) {
+        console.log(
+          'declarationAnalytics :>>>>>> ',
+          (declarationAnalytics as any).declaredAtLocationName
+        )
+
+        console.log(
+          'BEFOREconvertDotKeysToUnderscore---declarationAnalytics :>>>>>> ',
+          (convertDotKeysToUnderscore(declarationAnalytics) as any)
+            .declaredAtLocationName
+        )
+      }
 
       const actionWithFilteredDeclaration = {
         ...act,
@@ -206,21 +359,19 @@ async function upsertAnalyticsEventActions(
         declaredAt: declareAction ? declareAction.createdAt : null,
         registeredAt: registerAction ? registerAction.createdAt : null,
         annotation: convertDotKeysToUnderscore(annotation),
-        declaration: convertDotKeysToUnderscore(
-          precalculateAdditionalAnalytics(
-            action,
-            pickDeclarationAnalyticsFields(
-              actionAtCurrentPoint.declaration,
-              eventConfig
-            ),
-            eventConfig
-          )
-        )
+        declaration: convertDotKeysToUnderscore(declarationAnalytics)
       }
 
       allEventActions.push(actionWithFilteredDeclaration)
     }
   }
+
+  console.log(
+    'allEventActions :>> ',
+    allEventActions
+      .filter((ae) => ae.actionType === ActionType.DECLARE)
+      .map((act: any) => act.declaration.declaredAtLocationName as any)
+  ) // Only log DECLARE actions as they are the most important ones for analytics and usually the most enriched ones;
 
   if (allEventActions.length === 0) {
     return []
@@ -247,6 +398,7 @@ async function upsertAnalyticsEventActions(
       )
       .execute()
   }
+
   return
 }
 
